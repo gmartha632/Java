50f5cf9e666adc5c698e8041c66d09c28638cee9
gmartha632 <gmaruthamuthu144@gmail.com>
2025-01-20 21:37:31 +0530

    My first Java Program

---
+++ Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //This is my first Java program
+        System.out.println("Hello World");
+        System.out.println("It's really good");
+        //Primitive Data Type  int
+        int age = 30;
+        int year = 2025;
+        int quantity = 1;
+
+        System.out.print("The year is " + year);
+        //double
+
+        double price = 3000;
+        double gpa = 6.64;
+        double temperature = 40.2;
+
+        System.out.println("The temperature is " + temperature);
+
+        //char 
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println(symbol);
+
+// boolean
+        boolean isStudent = true;
+
+        if (isStudent) {
+            System.out.println("You are a student");
+
+        } else {
+            System.out.println("You are not a  student");
+        }
+
+        String name = "Muthu";
+        String car = "Mustang" ;
+        System.out.println("Hello " + name);
+        System.out.println("Your age is " + age + " years old");
+        System.out.println("Your gpa is "+ gpa);
+        System.out.print("Your grade is " + grade);
+
+     
+
+    }
+}
